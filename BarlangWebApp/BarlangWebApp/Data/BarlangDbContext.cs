f0b406903687887442d5c149d68a3b55aad3fddb
KBencee <kissbence225@gmail.com>
2025-09-30 08:32:25 +0200

    harmadik

---
+++ BarlangWebApp/BarlangWebApp/Data/BarlangDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BarlangWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarlangWebApp.Data
+{
+    public class BarlangDbContext : DbContext
+    {
+        public BarlangDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Barlang> barlangok { get; set; }
+    }
+}
